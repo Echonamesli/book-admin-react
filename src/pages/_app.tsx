83aff587fd8311fe0b46e6ea3fb96773ee5a4082
Echo4468 <321411510@qq.com>
2024-05-20 16:25:48 +0800

    2024-5-20

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { Layout } from '@/components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import "antd/dist/reset.css";
 
+//Component属性是活动的page，因此每当你在路由之间导航时，Component 都会更改为新的 page。因此，你发送给 Component 的任何属性都会被 page 接收。
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
